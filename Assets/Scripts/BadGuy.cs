444920f3f5d8de36ac4a1e2030411efd03ee75ee
Fedic <unknownman15@gmail.com>
2023-10-10 14:31:47 -0400

    In class test added. Fixed.

---
+++ Assets/Scripts/BadGuy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGuy
+{
+    public string name;
+    public int power;
+
+    public BadGuy(string newName, int newPower)
+    {
+        this.name = newName;
+        this.power = newPower;
+    }
+    
+}
---
+++ Assets/Scripts/TestList.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BadGuy bg1 = new BadGuy("Harvey", 50);
+        BadGuy bg2 = new BadGuy("Magneto", 100);
+        BadGuy bg3 = new BadGuy("Mary", 5);
+
+        List<BadGuy> badguys = new List<BadGuy>();
+        badguys.Add(bg1);
+        badguys.Add(bg2);
+        badguys.Add(bg3);
+
+        print("List size: " + badguys.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
